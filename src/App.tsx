7506d3c7df34c64015336687f26eef90d01fbf56
John Entwistle <j.entwistle76@gmail.com>
2026-01-18 20:58:24 -0500

    Add router and WIP homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import './App.css';
+import { Route, Routes } from 'react-router';
 import { Page } from './components/Page';
+import './App.css';
+
+const Router = () => (
+  <Routes>
+    <Route
+      element={
+        <Page displayTitle={false} pageId="2ea0c25cdd3980649eb4c157c0a41524" />
+      }
+      path="/"
+    />
+    <Route
+      element={<Page pageId="2e70c25cdd3981c28b55e02cc9e421ba" />}
+      path="/resume"
+    />
+  </Routes>
+);
 
 export const App = () => (
   <>
-    <Page pageId="2e70c25cdd3981c28b55e02cc9e421ba" />
+    <Router />
   </>
 );
