234540bc8c1bdf16657da35e2b867c65a1d3b925
John Entwistle <j.entwistle76@gmail.com>
2026-01-18 20:57:32 -0500

    Fetch top-level blocks immediately

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,35 +1,53 @@
 import { useQuery } from '@tanstack/react-query';
+import { getBlockData } from '../data/block';
 import { getPageData } from '../data/page';
-import { BlockChildren } from './BlockList';
+import { BlockItems } from './BlockList';
 import { Heading } from './Heading';
 import { LoadingOrError } from './Loading';
 import { Text } from './Text';
 
 type PageProps = {
+  displayTitle?: boolean;
   pageId: string;
 };
 
-export const Page = ({ pageId }: PageProps) => {
-  const { data, isLoading, error } = useQuery({
+export const Page = ({ pageId, displayTitle = true }: PageProps) => {
+  const {
+    data: pageData,
+    isLoading: pageLoading,
+    error: pageError,
+  } = useQuery({
     queryKey: ['pageData', pageId],
     queryFn: () => getPageData(pageId),
   });
 
-  if (isLoading || error || !data) {
-    return <LoadingOrError error={error} isLoading={isLoading} />;
+  const {
+    data: blockData,
+    isLoading: blockLoading,
+    error: blockError,
+  } = useQuery({
+    queryKey: ['blockData', pageId],
+    queryFn: () => getBlockData(pageId),
+  });
+
+  const isLoadingOrerror =
+    pageLoading || blockLoading || pageError || blockError;
+
+  if (isLoadingOrerror || !pageData) {
+    return <LoadingOrError error={pageError} isLoading={pageLoading} />;
   }
 
-  const { page } = data;
+  const { page } = pageData;
   return (
     <>
-      {page.title.type === 'title' && (
+      {displayTitle && page.title.type === 'title' && (
         <Heading level={1}>
           {page.title.title.map(textItem => (
             <Text key={textItem.plain_text} textItem={textItem} />
           ))}
         </Heading>
       )}
-      <BlockChildren parentId={pageId} />
+      {blockData && <BlockItems blocks={blockData.blocks} />}
     </>
   );
 };
