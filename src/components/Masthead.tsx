d7058b8a36796d641488610b7d2e3a17cdc1aac7
John Entwistle <j.entwistle76@gmail.com>
2026-01-25 14:57:35 -0500

    Remove unused component

--- src/components/Masthead.tsx
+++
@@ -1,8 +0,0 @@
-import { Box, type BoxProps } from './ui/Box';
-
-type MastheadProps = BoxProps<'div'>;
-export const Masthead = ({ as: __as, children, ...rest }: MastheadProps) => (
-  <Box as="div" {...rest}>
-    {children}
-  </Box>
-);
