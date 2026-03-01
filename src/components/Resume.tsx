a8d722aef3a14cd00283b1b72a73e3e74716e744
John Entwistle <j.entwistle76@gmail.com>
2026-02-28 20:30:10 -0500

    wip - temp static education and skills

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,4 +1,5 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { useDocumentTitle } from '#hooks/useDocumentTitle.ts';
 import { getPage, type ResumePageData } from '../data/page';
 import { useBreakpoint } from '../hooks/useBreakpoint';
 import {
@@ -8,20 +9,27 @@ import {
   resumeWrapper,
 } from '../styles/pages/Resume.css';
 import { Experience } from './Experience';
+import { SkillsFrontend } from './Skills';
 import { Box } from './ui/Box';
 import { Divider } from './ui/Divider';
 import { Flex } from './ui/Flex';
 import { Heading } from './ui/Heading';
 import { IconLink } from './ui/IconLink';
-import { List } from './ui/List';
 import { RichText } from './ui/RichText';
 import { TextBox } from './ui/TextBox';
+
+const Dot = () => (
+  <Box paddingX="200" style={{ fontSize: '8px', marginTop: '-1px' }}>
+    •
+  </Box>
+);
 
 type ResumeProps = {
   pageId: string;
 };
 
 export const Resume = ({ pageId }: ResumeProps) => {
+  useDocumentTitle('Senior Software Engineer | Resume');
   const { data: page } = useSuspenseQuery({
     queryKey: ['pageData', pageId],
     queryFn: () => getPage<ResumePageData>(pageId),
@@ -108,19 +116,29 @@ export const Resume = ({ pageId }: ResumeProps) => {
             width="50"
           />
 
-        <Box
-          paddingBlockStart={['0', '100']}
-          paddingInlineEnd={['0', '300']}
-          style={{ minWidth: '210px', maxWidth: '210px' }}
-        >
-          <Heading level={3}>{page.skills?.label}</Heading>
-          <List
-            items={page.skills?.multi_select.map(item => item.name)}
-            paddingBlockStart="200"
-            variant="inline"
-          />
-        </Box>
-      </Flex>
+          <Box
+            className={resumeRightCol}
+            paddingBlockStart={['0', '100']}
+            paddingInlineEnd={['0', '300']}
+          >
+            <SkillsFrontend page={page} />
+
+            <Divider marginY="600" width="50" />
+
+            <Box justifyContent="space-between">
+              <Heading level={3}>Education</Heading>
+              <Heading level={5} style={{ marginBlockEnd: '0.25em' }}>
+                Drexel University
+              </Heading>
+              <TextBox fontSize="50">Philadelphia, PA</TextBox>
+              <Flex justifyContent="space-between" paddingY="250">
+                <Flex alignItems="center">
+                  BS <Dot /> MIS
+                </Flex>
+                <div>2005-2010</div>
+              </Flex>
+            </Box>
+          </Box>
         </Flex>
       </div>
     </div>
